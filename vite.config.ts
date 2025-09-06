1ebdb474950fd8442a22e0bb4264f09cea863d3c
lblb475869 <lblb.475869@163.com>
2025-09-06 16:22:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',  // ⚠️ 这个很重要，确保 GitHub Pages 能加载到资源
 })
